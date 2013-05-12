0b868447faa2715e5eb78b303020cc498481e0ab
Gonzalo Garcia Accinelli <gonzalogacc@gmail.com>
2013-05-12 20:01:46 -0300

    Archivo base

---
+++ kriggingForTopo.py
@@ -0,0 +1,33 @@
+
+
+class kriggingForTopo:
+	""" Class doc """
+	
+	def __init__ (self, archivo_csv):
+		""" Class initialiser """
+		
+		self.archivo_csv = archivo_csv
+	
+	def cargarDesdeCSV (self):
+		""" 
+		Devuelve un arreglo de numpy dado un archivo csv de entrada
+		
+		Argumentos
+		---------------
+		archivo_csv: archivo con la informacion de los puntos a interpolar
+		
+		Devuelve
+		--------------
+		arreglo_np: arreglo de numpy de las coordenadas
+		"""
+		
+		return None 
+	
+	def generarVariograma ():
+		""" Function doc """
+		return None
+	
+	def interpolarSuperficie ():
+		""" Function doc """
+		
+	
